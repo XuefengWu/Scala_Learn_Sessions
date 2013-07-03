123284781bf0314aa03dbacfd40c2eac41895edb
Xuefeng.Wu <benewu@gmail.com>
2013-07-03 09:29:08 +0800

    simple file share app

---
+++ fileshare/app/controllers/Application.scala
@@ -0,0 +1,21 @@
+package controllers
+
+import play.api._
+import play.api.mvc._
+import java.io.File
+
+object Application extends Controller {
+
+  val ShareRoot = "/Users/twer/Downloads"
+  def file(path:String="") = Action {
+    val file = new File(s"$ShareRoot/$path")
+    if(file.isDirectory){
+      file.getParentFile
+      Ok(views.html.index(file,ShareRoot.length+1))
+    } else {
+      Ok.sendFile(file)
+    }
+
+  }
+  
+}
